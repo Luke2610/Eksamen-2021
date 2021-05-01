ef1dc79feccae79a540da22d72cff20ef7bac1a5
Lucas <luve20ac@student.cbs.dk>
2021-05-01 18:12:02 +0200

    commit

--- html/js/login.js
+++ html/js/login.js
@@ -17,7 +17,19 @@ loginButton.addEventListener("click",function(){
                     document.getElementById('gender').innerHTML = "Gender: " + data[3].value;
                     document.getElementById('birthdate').innerHTML = "Birthdate: " + data[4].value;
                     console.log(data);
-                    localStorage.setItem("user",data)
+                    localStorage.setItem("firstname",data[1].value)
+                    localStorage.setItem("lastname",data[2].value)
+                    localStorage.setItem("gender",data[3].value)
+                    localStorage.setItem("birthdate",data[4].value)
+                    localStorage.setItem("country",data[5].value)
+                    localStorage.setItem("city",data[6].value)
+                    localStorage.setItem("interestedInGender",data[7].value)
+                    localStorage.setItem("maxAge",data[8].value)
+                    localStorage.setItem("minAge",data[9].value)
+                    localStorage.setItem("biography",data[10].value)
+                    localStorage.setItem("email",data[11].value)
+                    localStorage.setItem("password",data[12].value)
+
                 })
             }
         )
