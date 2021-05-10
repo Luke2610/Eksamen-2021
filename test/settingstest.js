a5aec049fd72314b1b269048675ed10eeee8b343
Taha711 <72921177+Taha711@users.noreply.github.com>
2021-05-10 20:39:42 +0200

    commit

--- test/settingstest.js
+++ test/settingstest.js
@@ -3,15 +3,16 @@ var formatDate = require('../test');
 
 describe('formatDate()', function(){
     it('should get the date', function(){
-        //Arrange
+        //Arrange - opstil data
         var date = new Date(2021,1,1,12,0,0,0);
         
-        //act 
+        //act - opstil funktion
         var date1 = formatDate(date);
 
-        //Assert 
+        //Assert - sikrer at formatet fra date opfyldes
         var fullyear = "2021-01-01";
         /* expect(fullyear).to.Throw(date); */
+        // test af funktion - forventning af resultat
         expect(date1).to.equal(fullyear);
     })
 })
