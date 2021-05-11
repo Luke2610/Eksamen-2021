66b11f7645a4deffe3117f5d5caa799f94c9b710
Lucas <luve20ac@student.cbs.dk>
2021-05-11 13:12:47 +0200

    Merge branch 'main' of https://github.com/Luke2610/Eksamen-2021

--- delete_users_like/index.js
+++ delete_users_like/index.js
@@ -30,8 +30,8 @@ module.exports = async function (context, req) {
 async function get(context,req){
     try{
         let user_id = req.query.user_id;
-        let match = await db.delete_match(user_id)
-        let user = await db.delete_users_like(user_id)
+        let match = await db.delete_match(user_id) //delete match
+        let user = await db.delete_users_like(user_id) //delete like
         context.res = {
             body: user
         }
